6ca8ff2d6ae71a1ed8154cfbf66add9e55a1147d
Asiyat-d <143499065+Asiyat-d@users.noreply.github.com>
2024-09-30 15:33:25 +0500

    first commit

---
+++ WindowsFactory.java
@@ -0,0 +1,22 @@
+public class WindowsFactory implements GUIFactory {
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    public Window createWindow() {
+        return new WindowsWindow();
+    }
+}
+
+// Concrete Factory for Mac
+public class MacFactory implements GUIFactory {
+
+    public Button createButton() {
+        return new MacButton();
+    }
+
+
+    public Window createWindow() {
+        return new MacWindow();
+    }
+}
